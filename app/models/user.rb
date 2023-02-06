27b6fb1ba87722c3fb2955c54b5f19272f7e19f8
Prof. H <profh@cmu.edu>
2023-02-06 11:40:53 -0500

    Removing comments from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,6 @@ class User < ApplicationRecord
  # Most of the secure password machinery will be implemented using a single Rails built_in module
   # called `has_secure_password`, which we'll include in the User model as follows:
   has_secure_password
-
-# When included in a model as above, this one module adds the following functionality:
-    # 1- The ability to save a securely hashed `password_digest` attribute to the database: 
-    # The has_secure_password module will be responsible of creating the password_digest out of the password 
-    # and password confirmation entered by the user when signinup.
-    # 2- A pair of virtual attributes: password and password_confirmation, and their getters and setters 
-    # (as if we did the following:  attr_accessor :password, :password_confirmation)
-    # IMPORTANT NOTE: NEVER CREATE THESE TWO FIELDS WHEN YOU GENERATE THE MODEL.
-    # 3- An `authenticate` method that returns the user when the password is correct (and false otherwise)
-
-  # The only requirement for `has_secure_password` to work its magic is for the corresponding model to have 
-  # an attribute called `password_digest`. (The name digest comes from the terminology of cryptographic hash functions. 
-  # In this context, hashed password and password digest are synonyms.)
-  # In the case of the User model, this leads to the data model shown in the ERD of PATS.
-
-  # To make the password digest, `has_secure_password` uses a state-of-the-art hash function called `bcrypt`.
-  #  By hashing the password with bcrypt, we ensure that a hacker won't be able to log in to the site even if they
-  #  manage to obtain a copy of the database. 
-  # To use bcrypt in the sample application, we need to add the bcrypt gem to our Gemfile (Check line 29 of the Gemfile).
-
     
   # Relationships
   has_many :notes
